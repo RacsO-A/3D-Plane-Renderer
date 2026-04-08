988883415b823c85a5c4154bc64e76e290fcce3a
RacsO-A <oscar.abelanet@gmail.com>
2026-04-08 16:12:38 -0700

    Added rubiks cube defn

---
+++ rubiks.h
@@ -0,0 +1,41 @@
+#include 
+
+struct plane {
+	struct point3 corner;
+	struct point3 vertex[2];
+	struct point3 normal;
+	char symbol;
+};
+
+struct corner_p {
+	struct plane face[3];
+	struct plane internal[3];
+};
+
+struct edge_p {
+	struct plane face[2];
+	struct plane internal[2];
+};
+
+// Green front white top
+// corners and edges are in order from:
+//	top to bottom, tr clockwise
+// centers are top to bottom, tr clockwise
+struct cube {
+	// 8 corners
+	// 	3 face planes
+	//	3 internal planes
+	struct corner_p corners[8];
+	
+	// 12 edges
+	//	2 face planes
+	//	2 internal planes
+	struct edge_p edges[12];
+	
+	// 6 centers
+	//	1 face plane
+	struct plane centers[6];
+
+	// 0 if is currently doing a move, 1 when in static position
+	int is_static;
+};
